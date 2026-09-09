2d44b836dbd719f050f7b61911eb9cd570fb5af8
danial <danialshams07@gmail.com>
2026-09-10 01:13:28 +0330

    fix production build

--- types/index.ts
+++ types/index.ts
@@ -1,16 +1,14 @@
-// types/index.ts
 import { Role } from "@/generated/prisma/client";
 
 declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace NodeJS {
     interface ProcessEnv {
-      DATABASE_URL: string;
-      INSTAGRAM_CLIENT_ID: string;
-      INSTAGRAM_CLIENT_SECRET: string;
-      INSTAGRAM_REDIRECT_URI: string;
-      NEXTAUTH_SECRET: string;
-      NEXTAUTH_URL: string;
+      DATABASE_URL?: string;
+      INSTAGRAM_CLIENT_ID?: string;
+      INSTAGRAM_CLIENT_SECRET?: string;
+      INSTAGRAM_REDIRECT_URI?: string;
+      NEXTAUTH_SECRET?: string;
+      NEXTAUTH_URL?: string;
     }
   }
 }
