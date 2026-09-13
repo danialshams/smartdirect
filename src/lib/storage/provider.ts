990e2a194a73eb31dbdfb56a16d3dc76fadd68e8
danial shams <121979584+danialshams@users.noreply.github.com>
2026-09-13 23:36:12 +0330

    feat: support temporary test storage provider

--- src/lib/storage/provider.ts
+++ src/lib/storage/provider.ts
@@ -1,5 +1,6 @@
 import { cloudinaryStorageProvider } from "./cloudinary";
 import { localStorageProvider } from "./local";
+import { testStorageProvider } from "./test";
 
 export function getStorageProvider() {
   const provider = process.env.STORAGE_PROVIDER || "local";
@@ -10,6 +11,9 @@ export function getStorageProvider() {
 
     case "cloudinary":
       return cloudinaryStorageProvider;
+
+    case "test":
+      return testStorageProvider;
 
     default:
       throw new Error(`Storage provider "${provider}" پشتیبانی نمی‌شود.`);
