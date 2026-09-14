58d12518032be8108983455ca7bab7051ee119ee
danial shams <121979584+danialshams@users.noreply.github.com>
2026-09-14 13:21:38 +0330

    feat: integrate user tags into publishing dashboard

--- app/dashboard/publishing/page.tsx
+++ app/dashboard/publishing/page.tsx
@@ -2,15 +2,16 @@ import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 
 import { authOptions } from "@/lib/auth";
-import PublishingDashboard from "../../../components/dashboard/publishing/PublishingDashboard";
+import PublishingWithTags from "../../../components/dashboard/publishing/PublishingWithTags";
+
 export const dynamic = "force-dynamic";
 
 export default async function PublishingPage() {
-    const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-    if (!session?.user?.id) {
-        redirect("/login");
-    }
+  if (!session?.user?.id) {
+    redirect("/login");
+  }
 
-    return <PublishingDashboard />;
+  return <PublishingWithTags />;
 }
