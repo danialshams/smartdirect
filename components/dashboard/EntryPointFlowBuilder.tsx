79dd66aac21c08692e6efc69c176e0e7a71e4d79
danial shams <121979584+danialshams@users.noreply.github.com>
2026-09-20 21:32:49 +0330

    fix: provide trigger type for entry point flow

--- components/dashboard/EntryPointFlowBuilder.tsx
+++ components/dashboard/EntryPointFlowBuilder.tsx
@@ -1177,6 +1177,7 @@ export default function EntryPointFlowBuilder({
                                                     </div>
 
                                                     <AutomationFlowMessage
+                                                        triggerType="DM"
                                                         message={
                                                             message
                                                         }
